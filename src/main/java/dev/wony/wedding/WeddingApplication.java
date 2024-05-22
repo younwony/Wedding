e0ca3f94660e95357f6c20919a7e0a17a0474fb2
younwony <wony9324@naver.com>
2024-05-22 18:06:31 +0900

    init

---
+++ src/main/java/dev/wony/wedding/WeddingApplication.java
@@ -0,0 +1,13 @@
+package dev.wony.wedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeddingApplication.class, args);
+    }
+
+}
