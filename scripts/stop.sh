372fef181b3f46fdc2c16d763ac0f18214be8af6
younwony <wony9324@naver.com>
2024-06-16 00:21:06 +0900

    feat: deploy.yml 세팅 수정  - start, stop 변경

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# 애플리케이션 실행할 JAR 파일 경로 설정
+# 애플리케이션 실행할 WAR 파일 경로 설정
 PROJECT_ROOT="/home/ec2-user/build"
-JAR_FILE="$PROJECT_ROOT/wedding-0.0.1-SNAPSHOT.jar"
+WAR_FILE="$PROJECT_ROOT/wedding-0.0.1-SNAPSHOT.war"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
 # 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f $WAR_FILE)
 
 # 프로세스가 켜져 있으면 종료
-if [ -z $CURRENT_PID ]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
   echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
   kill -15 $CURRENT_PID
+  echo "$TIME_NOW > $CURRENT_PID 프로세스가 종료되었습니다." >> $DEPLOY_LOG
 fi
