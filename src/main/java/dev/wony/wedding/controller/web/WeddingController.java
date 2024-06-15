4f42afdd4f150bab77261ab42ddd305f49f2b067
younwony <wony9324@naver.com>
2024-06-15 23:49:28 +0900

    feat: CodeDeploy

--- src/main/java/dev/wony/wedding/controller/web/WeddingController.java
+++ src/main/java/dev/wony/wedding/controller/web/WeddingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WeddingController {
 
-    @GetMapping
+    @GetMapping("/wedding")
     public String index() {
         return "index";
     }
