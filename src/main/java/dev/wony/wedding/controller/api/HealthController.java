aeaa8c86c13d2b6c4b35ce5d295779475f1f6b12
younwony <wony9324@naver.com>
2024-06-16 14:10:30 +0900

    feat: health check 추가

---
+++ src/main/java/dev/wony/wedding/controller/api/HealthController.java
@@ -0,0 +1,14 @@
+package dev.wony.wedding.controller.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Healthy");
+    }
+}
